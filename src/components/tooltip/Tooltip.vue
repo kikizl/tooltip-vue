78f54a330e8c756573b2b1421f8a5dfd13e414e6
kikizl <zlcharon94@gmail.com>
2019-07-09 01:06:47 +0800

    Update packages

--- src/components/tooltip/Tooltip.vue
+++ src/components/tooltip/Tooltip.vue
@@ -2,7 +2,7 @@
     <span
         :data-label="label"
         :class="[newType, position, size, {
-            'b-tooltip': active,
+            'tooltip': active,
             'is-square': square,
             'is-animated': newAnimated,
             'is-always': always,
@@ -11,7 +11,6 @@
         }]"
         :style="{'transition-delay': `${delay}ms`}">
         <slot/>
-        <p>success quote</p>
     </span>
 </template>
 
@@ -64,3 +63,98 @@ export default {
     }
 }
 </script>
+<style lang="scss" >
+$tooltip-arrow-size: 5px;
+$tooltip-arrow-margin: 2px;
+
+$tooltip-multiline-sizes: (
+    small: 180px,
+    medium: 240px,
+    large: 300px
+);
+
+
+// Base
+.tooltip {
+    position: relative;
+    display: inline-flex;
+    &:before,
+    &:after {
+        position: absolute;
+        content: "";
+        opacity: 0;
+        visibility: hidden;
+        pointer-events: none;
+    }
+    &:before {
+        z-index: 889;
+    }
+    &:after {
+        content: attr(data-label);
+        width: auto;
+        padding: 0.35rem 0.75rem;
+        // border-radius: $radius-large;
+        font-size: 0.85rem;
+        // font-weight: $weight-normal;
+        box-shadow: 0px 1px 2px 1px rgba(0, 1, 0, 0.2);
+        z-index: 888;
+        white-space: nowrap;
+    }
+    &:not([data-label=""]):hover:before,
+    &:not([data-label=""]):hover:after {
+        transition-delay: inherit;
+        opacity: 1;
+        visibility: visible;
+    }
+    
+    &:not([data-label=""]).is-always {
+        &:before,
+        &:after {
+            opacity: 1;
+            visibility: visible;
+        }
+    }
+    &.is-multiline {
+        &:after {
+            display: flex-block;
+            text-align: center;
+            white-space: normal;
+        }
+    }
+    &.is-dashed {
+        // border-bottom: 1px dashed $grey-light;
+        cursor: default;
+    }
+    &.is-square {
+        &:after {
+            border-radius: 0;
+        }
+    }
+    &.is-animated {
+        &:before,
+        &:after {
+            // transition: opacity $speed $easing, visibility $speed $easing;
+        }
+    }
+
+
+    // .is-bottom{
+    //     top: calc(100% + 6px);
+    //     transform: translate(-50%,0);
+    //     margin-left:-50%;
+    //     &::before{
+    //         content:'';
+    //         position:absolute;
+    //         width: 0;
+    //         height: 0;
+    //         border-left:6px solid transparent;
+    //         border-right:6px solid transparent;
+    //         border-bottom:6px solid rgba($color: #000000, $alpha: .6);
+    //         top:-6px;
+    //         left:calc(50% - 3px);  
+    //     }
+    // }
+}
+
+</style>
+
--- src/pages/tooltip/Tooltip.vue
+++ src/pages/tooltip/Tooltip.vue
@@ -1,8 +1,21 @@
 <template>
     <div class="tooltip-demo">
-        demo here
-        <tooltip>
+        <p>demo here</p>
+
+        <tooltip label="Tooltip bottom"
+            position="is-bottom">
+            <button class="button is-dark">
+                Bottom
+            </button>
         </tooltip>
+
+        <!-- <tooltip label="I'm never closing"
+            :active="active"
+            always>
+            <button class="button">
+                Always
+            </button>
+        </tooltip> -->
     </div>
     
 </template>
